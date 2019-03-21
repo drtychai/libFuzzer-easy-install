580b918344ce6c2d5dc3c85b63d602ceb3eba86d
drtychai <justinangra@gmail.com>
2019-03-21 14:46:40 -0700

    fixed conditional

--- install_libfuzzer.sh
+++ install_libfuzzer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -e libFuzzer ]; then
-    (cd libFuzzer && git pull)
+if [ -e ../libFuzzer ]; then
+    (cd ../libFuzzer && git pull)
 else
     git clone https://chromium.googlesource.com/chromium/llvm-project/compiler-rt/lib/fuzzer ../libFuzzer
 fi
